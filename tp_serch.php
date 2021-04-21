d3191a39fb11467e15aaf9b5b0ab8cdc84a7dcbe
emmanuel3wa <emmanuel.bebe@3wa.io>
2021-04-21 19:03:08 +0200

    Add test anonymous new class

--- tp_serch.php
+++ tp_serch.php
@@ -12,27 +12,39 @@ class Search
 function serchSeq(array $seq, array $ocu):int{
     $countSeq = count($seq);
     $countOcu = count($ocu);
+    $index = 0;
 
     if($countOcu > $countSeq)
         return -1;
 
-    $index = 0;
     foreach ($seq as $key => $value) {
+
         if($index === $countOcu )
             return $key - $index;
+
         if ($value === $ocu[$index]){
             $index = $index + 1;
             var_dump($index);
         } else 
             $index = 0;
-        var_dump($index);
     }
 
     return -1;
 }
 
 function search_pos_seq_all(array $seq, array $ocu){
-    return new Search ($ocu, serchSeq($seq, $ocu));
+    return new Search($ocu, serchSeq($seq, $ocu));
+    
 }
+
+// function search_pos_seq_all(array $seq, array $ocu){
+//     return (new class(seqence: $ocu, pos: serchSeq($seq, $ocu))
+//     {
+//         public function __construct(
+//             public array $seqence,
+//         public int $pos
+//         ) {}
+//     });
+// }
 
 var_dump(search_pos_seq_all([1,5,8,6,7,55,7,5,5], [7,55]));
