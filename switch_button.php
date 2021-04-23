ddfdf2b803573e0a4b814f0225a0432d27be412a
emmanuel3wa <emmanuel.bebe@3wa.io>
2021-04-23 16:01:22 +0200

    switch button exo

---
+++ switch_button.php
@@ -0,0 +1,35 @@
+<?php
+
+class Lamp {
+    public function __construct(
+        private bool $is_On = false
+        )
+    {}
+
+    public function switchLamp(){
+        $this->is_On = !$this->is_On;
+        if ($this->is_On)
+            echo ("turn on" . PHP_EOL);
+        else
+            echo ("turn off". PHP_EOL);
+    }
+}
+
+
+class Button {                   
+    public function __construct(
+        private Lamp $lamp
+        )
+    {}
+
+    public function switchDevice(){
+        $this->lamp->switchLamp();
+    }
+}
+
+
+$lampbutton = new Button(new Lamp);
+echo $lampbutton->switchDevice(); 
+echo $lampbutton->switchDevice();
+echo $lampbutton->switchDevice(); 
+echo $lampbutton->switchDevice();
